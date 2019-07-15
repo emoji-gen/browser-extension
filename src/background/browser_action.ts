7adea08038e7335521c00c2bdce07f715b4019e4
Pine Mizune <pinemz@gmail.com>
2019-07-15 21:57:24 +0900

    fix: fix browser action's url

--- src/background/browser_action.ts
+++ src/background/browser_action.ts
@@ -5,7 +5,36 @@ const WEB_URL = 'https://emoji-gen.ninja'
 export function addListener() {
   chrome.browserAction.onClicked.addListener(
     (tab: chrome.tabs.Tab) => {
-      chrome.tabs.create({ url: WEB_URL })
+      const locale = getLocale()
+      const url = locale === 'ja' ? WEB_URL : WEB_URL + '/' + locale + '/'
+      chrome.tabs.create({ url })
     },
   )
+}
+
+// --------------------------------------------------------
+
+const DEFAULT_LOCALE = 'en'
+const SIMPLE_LOCALES = ['ja', 'ko', 'en']
+const SIMPLIFIED_CHINESE_COUNTRIES = ['cn', 'hans']
+const TRADITIONAL_CHINESE_COUNTRIES = ['tw', 'sg', 'hk', 'mo', 'hant']
+
+export function getLocale() {
+  const [ lang, country ] =
+    chrome.i18n.getUILanguage().toLowerCase().split('-')
+
+  if (SIMPLE_LOCALES.includes(lang)) {
+    return lang
+  }
+  if (SIMPLIFIED_CHINESE_COUNTRIES.includes(country)) {
+    return 'zh-Hans'
+  }
+  if (TRADITIONAL_CHINESE_COUNTRIES.includes(country)) {
+    return 'zh-Hant'
+  }
+  if (lang === 'zh') {
+    return 'zh-Hans'
+  }
+
+  return DEFAULT_LOCALE
 }
